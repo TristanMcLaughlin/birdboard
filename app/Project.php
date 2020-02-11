ed86ec67b3e60f26065765afb76da921d9cc69ba
Becca <becca.mclaughlin@laser.red>
2020-02-11 21:34:55 +0000

    Add project test

--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $guarded = [];
+
+    public function path (): string
+    {
+        return "/projects/{$this->id}";
+    }
 }
---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /**
+     * testAProjectExists
+     *
+     * @return void
+     */
+    public function testProjectHasAPath (): void
+    {
+        $project = factory('App\Project')->create();
+        
+        $this->assertEquals('/projects/' . $project->id, $project->path());
+    }
+}
